68c6fb579f658986929557b5acefa984016dad15
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-17 00:03:59 +0700

    Update page.tsx

--- src/app/dashboard/reports/monthly-attendance/page.tsx
+++ src/app/dashboard/reports/monthly-attendance/page.tsx
@@ -902,7 +902,7 @@ export default function MonthlyAttendanceReport() {
           )}
           <span className="font-medium">Download Laporan Excel</span>
         </button>
-        <hr className="border-t border-none mt-1 mb-4" />
+        <hr className="border-t border-none mb-4" />
       </div>
     </div>
   );
