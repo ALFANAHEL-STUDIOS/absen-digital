4524b47181d4c0ccee5d59dd88c22a1f943d2278
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-16 21:25:57 +0700

    Update page.tsx

--- src/app/dashboard/classes/page.tsx
+++ src/app/dashboard/classes/page.tsx
@@ -212,7 +212,7 @@ export default function Classes() {
               setFormData({ name: "", level: "1", room: "", teacherName: "" });
               setShowAddModal(true);
             }}
-            className="flex items-center justify-center w-full md:w-auto gap-2 bg-orange-500 text-white px-5 py-2.5 rounded-lg hover:bg-blue-600 active:bg-blue-700 transition-colors"
+            className="flex items-center justify-center w-full md:w-auto gap-2 bg-blue-500 text-white px-5 py-2.5 rounded-lg hover:bg-orange-600 active:bg-blue-700 transition-colors"
           >
             <Plus size={18} />
             Tambah Kelas
