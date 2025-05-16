034e120d5884ea35aebbd1dc62ac633a1b311172
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-16 12:51:02 +0700

    Update page.tsx

--- src/app/dashboard/reports/monthly-attendance/page.tsx
+++ src/app/dashboard/reports/monthly-attendance/page.tsx
@@ -811,12 +811,12 @@ export default function MonthlyAttendanceReport() {
             <p className="text-gray-600 font-bold">NPSN {schoolInfo.npsn}</p>
           </div>
           
-          <hr className="border-t border-gray-400 mt-1 mb-3" />
+          <hr className="border-t border-gray-400 mt-1 mb-6" />
 
           
-          <div className="text-center mb-4">
+          <div className="text-center mb-6">
             <h3 className="text-gray-600 uppercase">REKAP LAPORAN KEHADIRAN SISWA</h3>
-            <p className="text-gray-600 font-small">BULAN {formattedMonth.toUpperCase()}</p>
+            <p className="text-gray-600">BULAN {formattedMonth.toUpperCase()}</p>
           </div>
 
           
