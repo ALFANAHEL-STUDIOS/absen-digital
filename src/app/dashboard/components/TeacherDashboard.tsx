5b7ac5478602bfe6295d1d6342ed5341c4e8df14
ALFANAHEL <lehan.virtual@gmail.com>
2025-06-02 12:53:09 +0700

    Update TeacherDashboard.tsx

--- src/app/dashboard/components/TeacherDashboard.tsx
+++ src/app/dashboard/components/TeacherDashboard.tsx
@@ -172,7 +172,6 @@ export default function TeacherDashboard({
         </>}
     <hr className="border-t border-none mb-5" />
     <hr className="border-t border-none mb-5" />
-    <hr className="border-t border-none mb-5" />
-    <hr className="border-t border-none mb-5" />
+    
     </div>;
 }
